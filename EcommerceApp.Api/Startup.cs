76690a3f482d47decf2612924c9ee942bf4a5092
huyquyentran <huyquyent@gmail.com>
2020-04-07 15:01:03 +0700

    fix

--- EcommerceApp.Api/Startup.cs
+++ EcommerceApp.Api/Startup.cs
@@ -37,11 +37,18 @@ namespace EcommerceApp.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseCors();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(x =>
+                {
+                    x.AllowAnyHeader();
+                    x.AllowAnyMethod();
+                    x.AllowAnyOrigin();
+                });
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
