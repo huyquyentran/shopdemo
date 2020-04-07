9b8c57336767f308c5cddf3797528299739717cf
huyquyentran <huyquyent@gmail.com>
2020-04-07 14:50:54 +0700

    Fix cors

--- EcommerceApp.Api/Startup.cs
+++ EcommerceApp.Api/Startup.cs
@@ -37,6 +37,7 @@ namespace EcommerceApp.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
